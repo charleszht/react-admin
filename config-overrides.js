5af40938aca179c1dde6abebb19dbb4db189073e
zhangtong <charleszht@163.com>
2020-06-28 12:52:16 +0800

    修改域名

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: coding...
+ * @Version: 1.0.0版本
+ * @Author: 张彤
+ * @Date: 2020-03-04 17:50:01
+ * @LastEditors: 张彤
+ * @LastEditTime: 2020-06-28 12:47:35
+ */ 
 const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra')
 const path = require('path')
 const resolveAlias = dir => path.join(__dirname, '.', dir)
@@ -5,7 +13,7 @@ const theme = require('./theme')
 
 const proxyApi = {
   '/api': {
-    target: 'https://api.yesterdaypub.cn',
+    target: 'https://api.zhangtong.work',
     changeOrigin: true,
     secure: false,
     xfwd: false,
